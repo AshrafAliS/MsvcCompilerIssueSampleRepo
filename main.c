dbfcef761ae675d4353347d10eb8ed96457db165
Ashraf Ali S <ashraf.ali.s@intel.com>
2024-10-18 20:14:57 +0530

    include build folder as well

--- main.c
+++ main.c
@@ -2,9 +2,7 @@
 #include "Include/sample.h"
 
 int main() {
-    // printf("Main function\n");
     sampleone();
     SampleUsedFunctionOne();
-    // printf("HOWDO = 0x%llx\n", SampleFunctionAsmTwo ());
     return 0;
 }
